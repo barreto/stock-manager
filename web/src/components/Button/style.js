c4dd0837e0a00ca1b7bee9a2548e96139eb98b1c
Barreto <v.barreto.and@gmail.com>
2020-06-28 01:45:27 -0300

    feat: include on click event in custom button

--- web/src/components/Button/style.js
+++ web/src/components/Button/style.js
@@ -3,15 +3,17 @@ import styled from "styled-components";
 const defaultWidth = "120px";
 const defaultHeight = "120px";
 const defaultPadding = "8px 16px";
+const defaultBorderRadius = "10px";
+const defaultBorder = "1px solid #2b4d66";
 
 const CustomButton = styled.button`
+  outline: unset;
   color: #2b4d66;
   cursor: pointer;
   background: #d8e2f1;
-  border-radius: 10px;
-  border: 1px solid #2b4d66;
-  outline: unset;
   text-align: center;
+  border-radius: ${(props) => props.borderRadius || defaultBorderRadius};
+  border: ${(props) => props.border || defaultBorder};
   padding: ${(props) => props.padding || defaultPadding};
   width: ${(props) => props.widht || defaultWidth};
   height: ${(props) => props.height || defaultHeight};
