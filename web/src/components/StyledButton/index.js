95fc1149e53c1c6b7a8d29949da71fcfcd127153
Barreto <v.barreto.and@gmail.com>
2020-07-11 02:55:14 -0300

    feat: no backend communication vision

--- web/src/components/StyledButton/index.js
+++ web/src/components/StyledButton/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import CustomButton from "./style";
 
-const StyledButton = ({ height, width, padding, onClick, children }) => {
+const StyledButton = ({
+  height,
+  width,
+  disabled,
+  padding,
+  onClick,
+  children,
+}) => {
   return (
     <CustomButton
       padding={padding}
       height={height}
       width={width}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </CustomButton>
